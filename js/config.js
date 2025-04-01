d3963885ec2677dfbf61cf6ac53c4e8b8029786f
nguye381 <Thoai.H.Nguyen@Census.gov>
2025-04-01 15:17:37 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_koV49dOFK', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'User pool - 6d824b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '{"keys":[{"alg":"RS256","e":"AQAB","kid":"V+AYPgnE6YQ/h/NDxLL9KSGZ4yq2uThD40Z6poPpOFc=","kty":"RSA","n":"2Dojvrt8nn02Q5dtmC8EtpiyGkyZlTRRtbm0idNd3Xg8-KU93XKcCDwSg-N3kC29oRpDNfgp-ChEZmzt5w3cB5Cj7_xFy6dJIc11y2GU558151zSADLcCXhhO9pu7KiH1Rx_6GuBLXXe1q3C0VTJNefXfJ7111luyFv-bSqXnvP-Jc5CJBL3Hl72VyePvR4148ERWOcySOADN7wj1Tmy4YWwpjaPT8EuVd8VPEKV4kVycfuaIqq1qneXxK-Wf8nAulUD_Uuh4SX54offjPtc5APbOaslsWUY2KK5OyeaFVScIu6wsKXtNd_Ih4G7SKU9YulK4_Xc-J64yS3tF-Defw","use":"sig"},{"alg":"RS256","e":"AQAB","kid":"9XRam/HemHZmUbE1hr9xL3nmLssxZkMUWU5AI05/q94=","kty":"RSA","n":"stIuHLD4G99795QiADNcjlEx4oYy9jDMnb9PMuEr6VktgavXIJiJVgpirQRkh53rl7O6RyfpRDqU4uQbn4c9D7R0OCPVlUmn0Rw7RA5-J6vrspwHMoxFWicHBLCU64k1_XymTWxHKOpH1WWWza8lfZU5owfd-lMmg11eviOjSFf_SB7ZV9Ab2Hfy_jRjhMV_jatj40HRcSGTt1yB2ssPgnsZoOw6X3_-xgA2hy2-a2GxjTtG7b4h6WebWrCnub_AAKzZamJgWblqjKUhltekLtM_bz8alATumDVYMfg_BXEDrSnAwSq4n3THQoW0y6Kuuj5zpDCNIqBFXuoe5OMLNQ","use":"sig"}]}' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
